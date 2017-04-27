89aa55c85abb984a379e8f8b097e3160ba0a72ba
github username <github email address>
2017-04-27 10:57:58 -0400

    Done.

--- db/migrate/20150915202628_create_cats.rb
+++ db/migrate/20150915202628_create_cats.rb
@@ -1,4 +1,15 @@
 class CreateCats < ActiveRecord::Migration
+
+  def change
+    create_table :cats do |t|
+      t.string :name
+      t.integer :age
+      t.string :breed
+    end
+  end
+end
+
+
   def up
     create_table :cats do |t|
       t.string :name
@@ -6,7 +17,7 @@ class CreateCats < ActiveRecord::Migration
       t.string :breed
     end
   end
-  
+
   def down
     drop_table :cats
   end
